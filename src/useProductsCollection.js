404e2cfd76689fa86b65dc4ab458704879bf07ff
Tomeriko <tomer.shalev@gmail.com>
2023-02-26 11:34:26 +0200

    filters adjust

--- src/useProductsCollection.js
+++ src/useProductsCollection.js
@@ -1,4 +1,6 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, 
+         useMemo, useRef, 
+         useState } from "react";
 import { getShelf } from 'shelf-cms-sdk'
 import test_coll from './collection_test.json'
 
@@ -7,23 +9,61 @@ const q = {
   limit: -1,
 }
 
+/** 
+ * @typedef {object} ProductData
+ * @property {string[]} collections
+ * @property {string} video
+ * @property {number} price
+ * @property {string} handle a unique readable identifier
+ * @property {string[]} search
+ * @property {string[]} tags
+ * @property {string} desc
+ * @property {string[]} media
+ * @property {number} updatedAt
+ * @property {number} qty
+ * @property {string} title
+ * @property {number} createdAt
+ * @property {number} compareAtPrice
+ */
+
+/**
+ * @typedef {Object} FilterData
+ * @property {string} id id represented as {key}_{value}
+ * @property {string} name value or name
+ * @property {boolean} selected is it selected
+ */
+
+/**
+ * @param {ProductData[]} data
+ * 
+ */
 export const inferFilters = (data) => {
+  /**@type {Set<string>} */
   const tags = new Set()
+  /**@type {Map<string, number>} */
   const tags_counter = new Map()
+  /**@type {Map<string, FilterData[]>} */
   const map = new Map()
 
   // first create unique and count
-  data.forEach(it => {
-    it.tags.forEach(tag => {
-      tags.add(tag)
-      // let's count
-      if(!tags_counter.has(tag)) tags_counter.set(tag, 0)
-      tags_counter.set(tag, tags_counter.get(tag)+1)
-    })
-  });
+  data.forEach(
+    it => {
+      it.tags.filter(t => t.startsWith('filter:'))
+        // .map(t => t.split('filter:').pop())
+        .forEach(
+          tag => {
+            tags.add(tag)
+            // let's count
+            if(!tags_counter.has(tag)) 
+              tags_counter.set(tag, 0)
+            tags_counter.set(tag, tags_counter.get(tag)+1)
+          }
+        )
+    }
+  )
 
   // delete tags that appear in all the products
-  console.log('tags_counter ', tags_counter)
+  // console.log('tags_counter ', tags_counter)
   for (let k of tags_counter.keys())
     if (tags_counter.get(k)==data.length)
       tags.delete(k)
@@ -31,22 +71,36 @@ export const inferFilters = (data) => {
   // now build the filter map
   for (const tag of tags.values()) {
     const [k, v] = tag.split('_')
-    if(!map.has(k)) map.set(k, []) 
-    map.get(k).push({ id: `${k}_${v}`, name: v, selected: false})
+    if(!map.has(k)) 
+      map.set(k, []) 
+
+    map.get(k).push({ 
+      id: `${k}_${v}`, 
+      name: v, 
+      selected: false
+    })
   }
 
+  const filter_map_array = Array.from(map)
+  // console.log('filter_map_array ', filter_map_array)
   // return map
-  return Array.from(map)
+  return filter_map_array
 }
 
 function get_data() {
   return data_test_(2)
 }
 
+/**
+ * 
+ * @param {ProductData[]} data 
+ * @param {number} strategy 
+ * @returns 
+ */
 export const sort = (data, strategy) => {
   let sort_fn = (a, b) => true
   // users.sort((a, b) => a.firstname.localeCompare(b.firstname))
-  console.log(strategy);
+  // console.log(strategy);
   switch (parseInt(strategy)) {
     case 0: // price high
       sort_fn = (a, b) => -a.price + b.price
@@ -78,52 +132,34 @@ export const sort = (data, strategy) => {
   return data.sort(sort_fn)
 }
 
+/**
+ * 
+ * @param {ProductData[]} data 
+ * @param {[string, FilterData[]][]} filter_map 
+ * @returns 
+ */
 export const filter = (data, filter_map) => {
-  const filter_map_2 = filter_map.reduce((map, obj) => {
-    // array of values
-    // let vs = new Set(obj[1].map(it => it.selected ? it.id : -1))
-    let vs = new Set(obj[1].map(it => it.selected ? it.name : -1))
-    vs.delete(-1)
-    map.set(obj[0], vs)
-    return map
-  }, new Map())
+  
+  const filtered = data.filter(
+    product => {
+      return filter_map.reduce(
+        (prev, filter_vs) => {
+          // skip a filter, that is muted
+          if(filter_vs[1].every(fv => !fv.selected))
+            return prev
 
-  const tags_to_map = tags => {
-    const map = new Map()
-    tags.forEach(tag => {
-      const [k, v] = tag.split('_')
-      if(!map.has(k)) map.set(k, []) 
-      map.get(k).push(v)
-    })
-    return map
-  }
-
-  const filtered = data.filter(it => {
-    const it_tags = tags_to_map(it.tags)
-
-    let running = true
-    for (const [k, vs] of it_tags.entries()) {
-      // if previous val was false we collapse all,
-      // because it is a logical AND
-      if(!running) return false
-      const filter_vs = filter_map_2.get(k)
-      // if the category name is absent or nothing is elected we continue
-      if(filter_vs===undefined || filter_vs.size==0) continue
-      // otherwise let's find any, check using OR
-      const any_found = vs.reduce((p, c) => filter_vs.has(c) || p, false)
-      if(!any_found) return false
+          return prev &&
+                 product?.tags.some(
+                  t => filter_vs[1].some(
+                      fv => fv.id===t && fv.selected
+                    )
+                  )
+        }, true
+      )
     }
-    return true
-  })
+  )
 
   return filtered
-}
-
-function mul(arr, n=2) {
-  let res = arr
-  for (let ix = 0; ix < n; ix++)
-    res = [...res, ...arr]
-  return res
 }
 
 const useProductsCollection = (handle=undefined, default_sort=0) => {
@@ -137,13 +173,18 @@ const useProductsCollection = (handle=undefined, default_sort=0) => {
     async function fetchData() {
       setIsLoading(true)
       try {
-        const validHandle = false//handle!=='debug'
+        const validHandle = false //handle!=='debug'
         // console.log('handle ', handle);
         // console.log('validHandle ', validHandle);
-        const json = validHandle ? await getShelf().products.byCollectionHandle(handle) : test_coll
+        const json = validHandle ?
+               await getShelf().products.byCollectionHandle(handle) : 
+               test_coll
+
+        // console.log(json);
+
         // const json = test_coll
         all.current = {
-          products : mul(json.products, 1), 
+          products : json.products,
           collection : json.collection, 
         }
         setFilters(inferFilters(json.products))
@@ -157,10 +198,15 @@ const useProductsCollection = (handle=undefined, default_sort=0) => {
   }, [getShelf(), handle])
 
   const onFilterChange = useCallback(
-    (id, state) => { 
+    (id, state) => {
       setFilters(c => {
         c.forEach(([k, vs]) => {
-          vs.forEach(v => {if(v.id==id) v.selected=state})
+          vs.forEach(v => {
+            if(v.id===id) {
+              // console.log(id, state)
+              v.selected=state
+            }
+          })
         })
         return [...c]
       })
@@ -171,27 +217,32 @@ const useProductsCollection = (handle=undefined, default_sort=0) => {
     (idx) => {
       setSortOrder(idx)
     }
-  , [])
+    , []
+  )
 
   const products_filtered_sorted = useMemo(
     () => {
+      console.log('filters ', filters)
       let running = all.current?.products ?? []
-      if(filters) running = filter(running, filters)
+      if(filters) 
+        running = filter(running, filters)
       running = sort(running, sortOrder)
       return running
     }
-  , [filters, sortOrder]) 
+    , [filters, sortOrder]
+  ) 
 
-  return [ all.current.collection, 
-           products_filtered_sorted, 
-           loading, error, 
-          { 
-            sortOrder,
-            filters,
-            onFilterChange,
-            onSortChanged
-          }
-        ]
+  return [ 
+    all.current.collection, 
+    products_filtered_sorted, 
+    loading, error, 
+    { 
+      sortOrder,
+      filters,
+      onFilterChange,
+      onSortChanged
+    }
+  ]
 }
 
 export default useProductsCollection
