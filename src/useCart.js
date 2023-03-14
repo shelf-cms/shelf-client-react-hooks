1f09e3f0672ccd2544d0acbd2e1f61c771ceea1a
Tomeriko <tomer.shalev@gmail.com>
2023-03-14 15:14:19 +0200

    better cart

--- src/useCart.js
+++ src/useCart.js
@@ -4,32 +4,71 @@ import { useCallback, useEffect, useState } from "react"
 const useCart = () => {
   const [cart, setCart] = useState(getShelf().cart.get())
 
-  useEffect(() => {
-    return getShelf().cart.add_sub(setCart)
-  }, [getShelf()])
+  useEffect(
+    () => {
+      return getShelf().cart.add_sub(setCart)
+    }, []
+  )
   
-  const reset = useCallback(() => getShelf().cart.reset(), [getShelf()])
-  const add = useCallback((id, count, max_count, data) => {
-    getShelf().cart.addLineItem(id, count, max_count, data)
-  }, [getShelf()])
-  const update = useCallback((id, count, max_count) => {
-    getShelf().cart.updateLineItem(id, count, max_count)
-  }, [getShelf()])
-  const remove = useCallback((id) => {
-    console.log(getShelf().cart)
-    getShelf().cart.removeLineItem(id)
-  }, [getShelf()])
-  const get = useCallback((id) => {
-    getShelf().cart.getLineItem(id)
-  }, [getShelf()])
+  const reset = useCallback(
+    () => getShelf().cart.reset(), []
+  )
+  const add = useCallback(
+    /**
+     * @param {string} id 
+     * @param {number} count 
+     * @param {number} max_count 
+     * @param {ProductData} data 
+     * @returns {LineItem | undefined}
+     */
+    (id, count, max_count, data) => 
+      getShelf().cart.addLineItem(id, count, max_count, data)
+      , []
+  )
+  const update = useCallback(
+    /**
+     * @param {string} id 
+     * @param {number} count 
+     * @param {number} max_count 
+     * @param {ProductData} data 
+     * @returns {LineItem | undefined}
+     */
+    (id, count, max_count, data) =>
+    
+      getShelf().cart.updateLineItem(id, count, max_count, data)
+    , []
+  )
+  const remove = useCallback(
+    /**
+     * @param {string} id 
+     * @returns {LineItem | undefined}
+     */
+    (id) => 
+      getShelf().cart.removeLineItem(id)
+    , []
+  )
+  const get = useCallback(
+    /**
+     * @param {string} id 
+     * @returns {LineItem | undefined}
+     */
+    (id) => 
+      getShelf().cart.getLineItem(id)
+    , []
+  )
 
-  const total = useCallback(getShelf().cart.total, [getShelf()])
+
+  const total = useCallback(
+    getShelf().cart.total, []
+  )
 
   const controller = {
     add, get, remove, update, reset, total
   }
 
-  return [cart, controller]
+  return {
+    cart, controller
+  }
 }
 
 export default useCart
